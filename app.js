851102b51e1da754457a66f718e26d1fd87c76ca
anonimcore <kevinsalasjr@gmail.com>
2026-02-23 02:52:32 -0500

    Delete app.js

--- app.js
+++
@@ -1,9 +0,0 @@
-
-document.querySelectorAll("a").forEach(link => {
-    link.addEventListener("click", function(){
-        document.body.style.opacity = 0.6;
-        setTimeout(()=>{
-            document.body.style.opacity = 1;
-        },300);
-    });
-});
